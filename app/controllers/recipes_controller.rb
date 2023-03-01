330a40acfe7ccff83d8ae556a2839c9232300f19
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-01 10:51:26 +0100

    Create necessary view files and models

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,11 @@
 class RecipesController < ApplicationController
-  def index
-  end
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def destroy; end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
-  has_many :recipe_foods, dependent: :destroy
+  has_many :recipes through: :recipe_foods, dependent: :destroy
 
   validates :name, presence: true
   validates :measurement_unit, presence: true
