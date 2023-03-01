dc659923bc4af26c1999e3a1931c13a83b51a6e8
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-01 10:32:16 +0100

    Create other controllers, models and db migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipe_foods/index'
+  get 'public_recipes/index'
+  get 'general_shopping_lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
