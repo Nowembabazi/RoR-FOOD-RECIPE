3359c4544bab8a3a6adcf625c7bab38f81b1ae39
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-01 14:40:13 +0100

    Fixed confimation issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,15 @@ user3 = User.create!(name: 'Ariel Cam', email: 'ariella@gmail.com', password: 'M
 
 food1 = user1.foods.create!(name: 'Apple', price: 2500, measurement_unit: 'kg')
 food2 = user1.foods.create!(name: 'Pineapple', price: 5000, measurement_unit: 'kg')
+food3 = user1.foods.create!(name: 'Chicken breasts', price: 6800, measurement_unit: 'units')
+
+descripts1 = 'A hamburger or by apheresis burger, is a sandwich of German origin, consisting of two round-shaped buns generally
+  garnished with ground beef and raw vegetables, lettuce, tomato, onion, pickle, and sauce...'
+descripts2 = 'Pizza is a traditional cooking recipe of Italian cuisine, originating in Naples based on a flatbread of bread dough, topped
+  with various mixtures of ingredients and baked in the oven.'
+descripts3 = 'Crisps or potato chips, also known by the neologism of potato chips in Canada, are thin vegetable slices fried in oil,
+  which makes them crunchy. They are usually made of potatoes, but can also be made of vegetables or fruits.'
+
+recipe1 = user1.recipes.create!(name: 'Hamburger', description: "#{descripts1}", preparation_time: 1, cooking_time: 1.5, public: true)
+recipe2 = user2.recipes.create!(name: 'Pizza',  description: "#{descripts2}", preparation_time: 4.5, cooking_time: 9, public: true)
+recipe3 = user1.recipes.create!(name: 'chips', description: "#{descripts3}", preparation_time: 2.5, cooking_time: 6, public: true)
