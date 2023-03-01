be816dc1f29c958d498c5dae3958df744577bf67
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-03-01 10:24:23 +0300

    changes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,7 +8,7 @@ class FoodsController < ApplicationController
 
   # GET /foods/1 or /foods/1.json
 
-  def show;end
+  def show; end
 
   # GET /foods/new
 
@@ -18,7 +18,7 @@ class FoodsController < ApplicationController
 
   # GET /foods/1/edit
 
-  def edit;end
+  def edit; end
 
   # POST /foods or /foods.json
   def create
