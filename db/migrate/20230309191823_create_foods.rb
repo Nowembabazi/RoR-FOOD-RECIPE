12bd0e85192528bf048f4a420c3a95c6c8c62e18
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-03-03 18:31:39 +0300

    App tests

--- db/migrate/20230309191823_create_foods.rb
+++ db/migrate/20230309191823_create_foods.rb
@@ -2,8 +2,9 @@ class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
       t.string :name
-      t.string :measurement
+      t.string :measurement_unit
       t.integer :price
+      t.integer :quantity
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
