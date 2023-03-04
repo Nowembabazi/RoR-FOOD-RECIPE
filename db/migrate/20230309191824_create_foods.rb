774e4496038ece5b40b5342c038d803c2ae93ca1
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-04 09:20:55 +0100

    Attempted fix for the ingredient views

--- db/migrate/20230309191824_create_foods.rb
+++ db/migrate/20230309191824_create_foods.rb
@@ -3,7 +3,8 @@ class CreateFoods < ActiveRecord::Migration[7.0]
     create_table :foods do |t|
       t.string :name
       t.string :measurement
-      t.integer :price
+      t.decimal :price
+      t.integer :quantity
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_191823) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_191824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement"
-    t.integer "price"
+    t.decimal "price"
+    t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
